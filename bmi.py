1dbdf64c52764bcd3dd892b3865b79e2034f5366
402-23-Jirat <54375@yupparaj.ac.th>
2026-09-09 14:45:03 +0700

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -6,14 +6,15 @@ st.write("กรอกข้อมูลน้ำหนักและส่ว�
 weight = st.number_input("กรอกน้ำหนัก (กิโลกรัม):", minvalue=1.0, value=1.0)
 height_cm = st.number_input("กรอกความสูง (เซนติเมตร):", minvalue=1.0, value=1.0)
 
-if st.button("ผลที่ได้"):
+if st.button("ผลที่ได้"):(
 
    height_m = height_cm / 100
+   
    bmi = weight / (height_m ** 2)
 
 st.write("---")
 st.header(f"ค่า BMi คือ:{bmi:.2f}**")
-
+)
 if bmi < 18.5:
     st.warning("น้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
 elif 18.5 <= bmi < 25.0:
