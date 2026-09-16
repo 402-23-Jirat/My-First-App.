767eb3f024e8efa04d310698e9e2ac9fee541432
402-23-Jirat <54375@yupparaj.ac.th>
2026-09-16 15:01:29 +0700

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -3,34 +3,28 @@ import streamlit as st
 st.markdown("# :red[คำนวณค่าดัชนีมวลกาย BMI]")
 st.write("กรอกข้อมูลน้ำหนักและส่วนสูง")
 
-weight = st.number_input("กรอกน้ำหนัก (กิโลกรัม):", min_value=1.0, value=1.0)
-height_cm = st.number_input("กรอกความสูง (เซนติเมตร):", min_value=1.0, value=1.0)
+weight = st.number_input("กรอกน้ำหนัก (กิโลกรัม):", min_value=1.0, value=50.0)
+height_cm = st.number_input("กรอกความสูง (เซนติเมตร):", min_value=1.0, value=160.0)
 
 if st.button("ผลที่ได้"):
- 
-   height_m == height_cm / 100
-   bmi == weight / (height_m ** 2)
+
+    height_m = height_cm / 100
+    bmi = weight / (height_m ** 2)
+
+    st.write("---")
+    st.header(f"ค่า BMI คือ: **{bmi:.2f}**")
 
 
-st.write("---")
-
-st.header(f"ค่า BMI คือ: **{bmi:.2f}**")
-
-if bmi < 18.5:
-    
-   st.warning("น้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
-
-elif 18.5 <= bmi < 25.0:
-    
-   st.info("ปกติ (สุขภาพดี)")
- 
-elif 23.0 <= bmi < 25.0:
-    
-   st.info("น้ำหนักเริ่มเกิน (ท้วม)")
-
-else :
-    
-   st.error("อ้วน (อ้วน)")
+    if bmi < 18.5:
+        st.warning("น้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
+    elif 18.5 <= bmi < 23.0:
+        st.success("ปกติ (สุขภาพดี)")
+    elif 23.0 <= bmi < 25.0:
+        st.info("น้ำหนักเริ่มเกิน (ท้วม)")
+    elif 25.0 <= bmi < 30.0:
+        st.warning("อ้วน (ระดับ 1)")
+    else:
+        st.error("อ้วนมาก (ระดับ 2)")
 
 st.divider()
 st.write("นายจิรัศมิ์ จิกเวียง เลขที่ 23 ม.4/2")
